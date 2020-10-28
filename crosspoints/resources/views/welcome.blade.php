75378694b0011141dd316f0dc81da9840802407d
THOMASter <thomas@bezemer.net>
2020-10-28 11:18:43 +0100

    edited welcome blade

--- crosspoints/resources/views/welcome.blade.php
+++ crosspoints/resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
             <div class="">
                 <div class="title m-b-md">
                     Crosspoints B<i class="fa fa-heart" style="color:red;"></i>
+                    veel liefs van thomas
                 </div>
 
                 <div class="links">
