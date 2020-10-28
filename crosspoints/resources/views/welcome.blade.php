aaf7e46de10049d187e86b8e08bb2e4c82f2ef68
luuksg12 <0966746@hr.nl>
2020-10-28 11:10:52 +0100

    Luuk branch test

--- crosspoints/resources/views/welcome.blade.php
+++ crosspoints/resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
             <div class="">
                 <div class="title m-b-md">
                     Crosspoints B<i class="fa fa-heart" style="color:red;"></i>
+                    <p>Wow luuk branch test</p>
                 </div>
 
                 <div class="links">
