6776db36591d85ca49e5248d5cf54e88650b99b4
gijsjeuwu <57947605+gijsjeuwu@users.noreply.github.com>
2020-11-12 14:42:07 +0100

    made the crosspoints info page

--- crosspoints/resources/views/welcome.blade.php
+++ crosspoints/resources/views/welcome.blade.php
@@ -1,13 +1,51 @@
 @extends('layouts.app')
 @section('content')
-    <body class="content">
+<body class="content">
     <div class="flex-center position-ref full-height">
         <div class="">
             <div class="title m-b-md">
-                Crosspoints B<i class="fa fa-heart" style="color:red;"></i>
+                Crosspoints B
+           </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-20">
+                        <div class="card">
+                           <h2 class="d-flex justify-content-center login-header info-header">
+                                Ongewenst gedrag op de werkvloer is afschuwelijk om mee te maken. Het kan bij jou voorkomen, maar ook bij een van je collega's. Wat kun je hier tegen doen?
+                           </h2>
+                            <div class="card-body login-body">
+                               <p class="text-center">
+                                    Crosspoints B is een website waarop je ongewenst gedrag op de werkvloer kunt melden. Als je last hebt van ongewenst gedrag kun je verschillende dingen doen.
+                                    Zo kun je tips bekijken, de 'moet-ik-melden' test doen of een melding maken. Op de informatiepagina kun je verschillende soorten ongewenst gedrag zien,
+                                    met tips en links erbij. Mochten deze tips en links niet genoeg zijn, dan hun je er voor kiezen om de 'moet-ik-melden' test te doen.
+                                    In deze test beantwoord je verschillende vragen, waardoor wij je verschillende dingen kunnen adviseren. Zo kunnen we laten weten dat het niet nodig is om een melding te maken
+                                    of we laten je weten dat we het zeker aanraden om een melding te maken. Bij alle adviezen kun je natuurlijk zelf bepalen wat je daadwerkelijk wilt doen.
+                                    Je kunt er ook voor kiezen om meteen een melding te maken.
+                               </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p>Hier komt informatie over de site</p>
+            <br>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-20">
+                        <div class="card">
+                            <h2 class="d-flex justify-content-center login-header info-header">
+                                    Hoe werkt het melden?
+                            </h2>
+                             <div class="card-body login-body">
+                                <p class="text-center">
+                                    Als je ongewenst gedrag wilt melden, moet je verschillende dingen invullen. Zo moet je de situatie beschrijven, bepalen hoe je de situatie hebt ervaart en sinds wanneer je deze situatie ongeveer ervaart.
+                                    Ook heb je de optie om zelf een vertrouwenspersoon te kiezen. Zo kun je kiezen voor een man of een vrouw.
+                                </p>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    </body>
+</body>
 @endsection
