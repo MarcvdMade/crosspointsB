fe31d73f6b864ca1e620836b69f6ef8f457735e2
D-Abdi <56637726+D-Abdi@users.noreply.github.com>
2020-11-19 10:44:38 +0100

    Meld pagina is bijgewerkt.

--- crosspoints/resources/views/melding/meld.blade.php
+++ crosspoints/resources/views/melding/meld.blade.php
@@ -11,31 +11,40 @@
                      <form method="POST" action="{{route('meld.create')}}">
                             @csrf
                             <div class="form-group">
-                                <label for="situation">Situatie:</label>
-                                <textarea name="situation" class="form-control" rows="3" placeholder="Omschrijf de situatie zo duidelijk mogelijk"></textarea>
+                                <label for="situation" class="d-flex justify-content-center info-link-label">Situatie:</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
+                                    <textarea name="situation" class="form-control" rows="3" placeholder="Omschrijf de situatie zo duidelijk mogelijk"></textarea>
+                                </div>
                                 @error('situation')
                                 <p>{{$errors->first('situation')}}</p>
                                 @enderror
                             </div>
 
                             <div class="form-group">
-                                <label for="since">Sinds wanneer is deze situatie aan de gang?</label>
+                                <label for="since" class="d-flex justify-content-center info-link-label">Sinds wanneer is deze situatie aan de gang?</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
                                 <input type="text" name="since" class="form-control" />
                                 @error('since')
                                 <p>{{$errors->first('since')}}</p>
                                 @enderror
+                                </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="experience">Hoe heeft u deze situatie ervaart? 0 is het doet me niks 10 is het meest heftige voor u</label>
-                                <textarea name="experience" class="form-control" rows="3" placeholder="s"></textarea>
+                                <label for="experience" class="d-flex justify-content-center info-link-label">Hoe heeft u deze situatie ervaren?</label>
+                                <p class="d-flex justify-content-center">0 = niet erg / 10 = zeer heftig</p>
+                                <div class="col-12 d-flex justify-content-center">
+                                <input type="range" class="form-control-range" id="myRange" min="0" max="10" value="5">
                                 @error('experience')
-                                <p>{{$errors->first('experience')}}</p>
-                                @enderror
+                                    <p>{{$errors->first('experience')}}</p>
+                                    @enderror
+                                </div>
+                                <p class="d-flex justify-content-center info-link-label" style="font-size: 32px"><span id="demo"></span></p>
                             </div>
 
                             <div class="form-group">
-                                <label for="counselor">Kies een vertrouwenspersoon:</label>
+                                <label for="counselor" class="d-flex justify-content-center info-link-label">Kies een vertrouwenspersoon:</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
                                     <select class="form-control" id="counselor" name="counselor">
                                         <option value="Geen voorkeur">Geen voorkeur</option>
                                         <option value="Man">Man</option>
@@ -44,10 +53,12 @@
                                 @error('counselor')
                                 <p>{{$errors->first('counselor')}}</p>
                                 @enderror
+                                </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="contact">Hoe wilt u dat er contact met u opgenomen wordt?</label>
+                                <label for="contact" class="d-flex justify-content-center info-link-label">Hoe wilt u dat er contact met u opgenomen wordt?</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
                                     <select class="form-control" id="contact" name="contact">
                                         <option value="E-mail">E-mail</option>
                                         <option value="Telefoon">Telefoon</option>
@@ -55,25 +66,31 @@
                                 @error('contact')
                                 <p>{{$errors->first('contact')}}</p>
                                 @enderror
+                                </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="phone">Telefoon nummer:</label>
+                                <label for="phone" class="d-flex justify-content-center info-link-label">Telefoon nummer:</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
                                 <input type="text" name="phone" class="form-control" />
                                 @error('phone')
                                 <p>{{$errors->first('phone')}}</p>
                                 @enderror
+                                </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="conditions">U dient akkoord te gaan met de algemene voorwaarden als u het formulier wilt opsturen </label>
-                                <input type="checkbox" name="conditions" required>
+                                <label for="conditions" class="d-flex justify-content-center info-link-label">Ik ga akkoord met de algemene voorwaarden</label>
+                                <div class="col-12 d-flex justify-content-center info-link-label">
+                                <input type="checkbox" name="conditions" style="width: 30px;height: 30px;" required>
                                 @error('conditions')
                                 <p>{{$errors->first('conditions')}}</p>
                                 @enderror
+                                </div>
                             </div>
-
-                            <button type="submit" class="btn btn-success" >Verzend</button>
+                            <div class="d-flex justify-content-center">
+                            <button type="submit" class="info-create-button">Verzend</button>
+                            </div>
                         </form>
                     </div>
                     </div>
@@ -81,5 +98,15 @@
             </div>
         </div>
     </div>
+
+    <script>
+        var slider = document.getElementById("myRange");
+        var output = document.getElementById("demo");
+        output.innerHTML = slider.value;
+
+        slider.oninput = function() {
+            output.innerHTML = this.value;
+        }
+    </script>
 
 @endsection
